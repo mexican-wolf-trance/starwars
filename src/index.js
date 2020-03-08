af1a07400cd8e731ea7572bfbce00b952a0ebf67
mexican-wolf-trance <bargain.ghost@yandex.com>
2020-03-08 15:52:41 -0500

    Starting from scratch

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+
+function HelloWorld()
+{
+    return ( <div>I'm finally making progress....</div> );
+}
+ReactDOM.render(<HelloWorld />, document.querySelector("#root"));
